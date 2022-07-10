a3c535e5234d0c780917a17821bd7b3fa8674d10
Mark Linn <marktlinn@gmail.com>
2022-07-10 12:47:58 +0200

    added ability to find people from the list through thr URL search bar, 404 output with text if person not found

--- part4_exercises/server.js
+++ part4_exercises/server.js
@@ -51,6 +51,19 @@ app.get("/info", (req,res)=>{
   res.send(`<p>Phonebook has info for ${dataIdCounter(data)} people</p>\n\n<p>${dateLog()}</p>`);
 })
 
+app.get("/api/persons/:num", (req, res)=>{
+  let idNum = req.params.num;
+  // res.send(`<h1>${idNum}</h1>`);
+  console.log(data.find(item=> item["id"] ==idNum))
+  if(!data.find(item=> item["id"] ==idNum)){
+    res.status(404).send(`<h1>Person Not Found</h1>`);
+  }
+  else if (data.find(item=> item["id"] ==idNum)) {
+    res.send(`<h1>${idNum}: ${JSON.stringify(data.find(item=> item["id"]== idNum))}</h1>`)
+  }
+
+})
+
 app.listen(PORT, ()=>{
     console.log(`Listening on port: ${PORT}`);
 })
