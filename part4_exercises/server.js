8d5c548f299de895e273f94481d30b171aeb485d
Mark Linn <marktlinn@gmail.com>
2022-07-10 16:26:31 +0200

    server now operationally accepting post and delete requests

--- part4_exercises/server.js
+++ part4_exercises/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const body = require("body-parser")
 const PORT = 3001;
 
 const data = [
@@ -25,11 +26,11 @@ const data = [
     },
     { 
       "id": 5,
-      "name": "Jeap Pierre Baptiste", 
+      "name": "Jean Pierre Baptiste", 
       "number": "42-12-235564"
     }
 ]
-
+app.use(express.json())
 app.get("/api/persons", (req,res)=>{
     res.json(data);
 })
@@ -43,7 +44,6 @@ function dataIdCounter(data) {
 }
 
 const dateLog = ()=>{
-  console.log('called')
   return new Date().toUTCString();
 }
 
@@ -53,15 +53,63 @@ app.get("/info", (req,res)=>{
 
 app.get("/api/persons/:num", (req, res)=>{
   let idNum = req.params.num;
-  // res.send(`<h1>${idNum}</h1>`);
-  console.log(data.find(item=> item["id"] ==idNum))
   if(!data.find(item=> item["id"] ==idNum)){
-    res.status(404).send(`<h1>Person Not Found</h1>`);
+    res.status(404).send(`<h1>Person Not Found</h1>`).end();
   }
   else if (data.find(item=> item["id"] ==idNum)) {
     res.send(`<h1>${idNum}: ${JSON.stringify(data.find(item=> item["id"]== idNum))}</h1>`)
   }
+})
 
+app.delete("/api/delete/persons/:num", (req,res)=>{
+  let idNum = req.params.num;
+  if(!data.find(item=> item["id"] == idNum)){
+    res.status(404).send(`<h1>Nothing deleted: Person Not Found</h1>`).end();
+  }
+  else {
+    if(data.filter(item=> item["id"] == idNum)){
+      delete data[idNum-1]
+    }
+    res.status(204).send(`<h1>ID: ${idNum} deleted</h1>`).end();
+    console.log(`ID: ${idNum} deleted`)
+  }
+})
+
+function generateId(min=5, max = 2000){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function checkName(newName){
+  if(data.find(person=> person.name == newName) != undefined) return true
+
+}
+console.log(checkName('Jean Pierre Baptiste'))
+
+app.post("/api/persons", (req,res)=>{
+  const person = req.body;
+  console.log(person)
+  if(person.name == '' || person.number == ''){
+    console.log(person.name)
+    return res.status(400).json({
+      error: "content missing"
+    });
+  } else if (checkName(person.name) == true){
+    return res.status(400).json({
+      error: "Name already exists"
+    })
+  }
+
+  let updatePersons = {
+    id: generateId(),
+    name: person.name,
+    number: person.number
+  }
+
+  data.push(updatePersons)
+
+  res.json(data)
 })
 
 app.listen(PORT, ()=>{
